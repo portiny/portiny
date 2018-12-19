c3c5e6094becbe6e8ac9e7719fccc7e753e5c0f1
Tomas Pilar <email@tomaspilar.cz>
2018-12-19 22:38:09 +0100

    [All]: optimize composer deps

--- packages/rabbitmq/tests/Adapter/Nette/DI/ConsoleExtensionTest.php
+++ packages/rabbitmq/tests/Adapter/Nette/DI/ConsoleExtensionTest.php
@@ -3,8 +3,6 @@
 namespace Portiny\RabbitMQ\Tests\Adapter\Nette\DI;
 
 use Portiny\RabbitMQ\BunnyManager;
-use Portiny\RabbitMQ\Command\ConsumeCommand;
-use Portiny\RabbitMQ\Command\DeclareCommand;
 use Portiny\RabbitMQ\Tests\AbstractContainerTestCase;
 
 final class ConsoleExtensionTest extends AbstractContainerTestCase
@@ -13,11 +11,5 @@ final class ConsoleExtensionTest extends AbstractContainerTestCase
 	{
 		$bunnyManager = $this->container->getByType(BunnyManager::class);
 		$this->assertInstanceOf(BunnyManager::class, $bunnyManager);
-
-		$consumeCommand = $this->container->getByType(ConsumeCommand::class);
-		$this->assertInstanceOf(ConsumeCommand::class, $consumeCommand);
-
-		$declareCommand = $this->container->getByType(DeclareCommand::class);
-		$this->assertInstanceOf(DeclareCommand::class, $declareCommand);
 	}
 }
